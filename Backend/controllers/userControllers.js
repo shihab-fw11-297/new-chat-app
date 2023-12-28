6b42a340b61ded43024ce646f64aff8c0f6c0cd9
shihab shaikh <123071760+ShihabLahagora@users.noreply.github.com>
2023-12-28 20:05:17 +0530

    create user controller base

---
+++ Backend/controllers/userControllers.js
@@ -0,0 +1,19 @@
+const asyncHandler = require("express-async-handler");
+
+const allUsers = asyncHandler(async (req, res) => {
+
+
+})
+
+const authUser = asyncHandler(async (req, res) => {
+
+
+})
+
+
+const registerUser = asyncHandler(async (req, res) => {
+
+
+})
+
+module.exports = { allUsers, registerUser, authUser };
